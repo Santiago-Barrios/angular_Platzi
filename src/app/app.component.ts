537a803871a5340c435ea7422eda97257b961438
Santiago Barrios <santiagobarriosgrijalba.08@gmail.com>
2020-07-03 15:48:05 -0500

    Uso de *ngFor add and delete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Platzi-Store';
+
+  items = ['Nicolas', 'Santiago', 'David'];
+
+  addItem() {
+    this.items.push( 'nuevo item' );
+  }
+
+  deleteItem( index: number ) {
+    this.items.splice(index, 1);
+  }
+
 }
