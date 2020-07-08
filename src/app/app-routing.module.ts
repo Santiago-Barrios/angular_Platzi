adbf8d5323e1612d64a8d61b80424a77f066b6aa
Santiago Barrios <santiagobarriosgrijalba.08@gmail.com>
2020-07-08 17:20:18 -0500

    Modularizando PageNotFound

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { LayoutComponent } from './layout/layout.component';
 
 
@@ -39,7 +38,7 @@ const routes: Routes = [
   },
   {
     path: '**',
-    component: PageNotFoundComponent,
+    loadChildren: () => import('./page-not-found/page-not-found.module').then( m => m.PageNotFoundModule ),
   }
 ];
 
