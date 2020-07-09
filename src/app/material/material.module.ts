c7eeb5b1dbd126c3fee3ecdbf5753f5b3fa64d64
Santiago Barrios <santiagobarriosgrijalba.08@gmail.com>
2020-07-09 00:01:19 -0500

    Creando el diseño del Footer y estilos a product usando cart de material

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -5,6 +5,8 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatBadgeModule} from '@angular/material/badge';
+import {MatCardModule} from '@angular/material/card';
+
 
 @NgModule({
   declarations: [],
@@ -14,13 +16,14 @@ import {MatBadgeModule} from '@angular/material/badge';
     MatToolbarModule,
     MatIconModule,
     MatBadgeModule,
+    MatCardModule,
   ],
   exports: [
     MatButtonModule,
     MatToolbarModule,
     MatIconModule,
     MatBadgeModule,
-
+    MatCardModule,
   ]
 })
 export class MaterialModule { }
